b7953d8a0f54b3cffa2a9bc94be83c6ce22313fe
Tony Messias <tonysm@hey.com>
2021-03-13 02:28:22 -0300

    Fixes homepage

--- source/index.blade.php
+++ source/index.blade.php
@@ -20,18 +20,18 @@
                     of projects: from content platforms, to booking websites, to real-estate systems, and even a bit of
                     IoT working on a building management platform (yes, automating all sorts of things in a building).
                 </p>
-                <p>As part-time DevOps person, I many deploying and configuring local development environments on a lot
-                    of different projects. We had a lot different projects there, so I had to learn a bunch of different
+                <p>As part-time DevOps person, I managed to deploy and configure local development environments on a lot
+                    of different stacks. I was part of a bunch of different projects, so I interacted with a set of different
                     OPS tools and platforms, such as Docker, Kubernetes, Laravel Forge, Laravel Envoyer, DigitalOcean,
                     AWS (including, but not limited to, some non-conventional services like the AWS Transcribe and AWS
                     IoT Core), Azure, GCP, among other things.</p>
                 <p>And before that, I worked as a Back-end Engineer at Gabstr, a location-based social network. Built
-                    the APIs we used and a geo-location based content platform on top of Redis and PostGIS.</p>
+                    the APIs we used there and a geolocation based content platform on top of Redis and PostGIS.</p>
             </section>
 
             <section class="space-y-4">
                 <h2 class="text-3xl font-mono font-semibold mb-4">$ cat /var/courses.txt</h2>
-                <p>I've been working on some video content, you can find them here:</p>
+                <p>I've been working on video content, you can find them here:</p>
 
                 <ul class="list-disc ml-5">
                     <li class="space-x-2"><a href="/courses/hotwire-laravel">Hotwire & Laravel</a> <span
@@ -39,13 +39,14 @@
                     <li class="space-x-2"><a href="/courses/kubernetes-for-laravel-developers">Kubernetes for Laravel
                             Developers</a> <span
                                 class="text-sm px-2 py-1 rounded bg-blue-100 text-blue-800">#free</span></li>
-                </ul>
+		</ul>
+		<p>You can find more videos on my <a href="https://www.youtube.com/c/TonyMessiasDev">YouTube channel</a>.</p>
             </section>
 
             <section class="space-y-4">
                 <h2 class="text-3xl font-mono font-semibold">$ cat /var/thoughts.txt</h2>
                 <p>Writing is how we consolidate our learnings. I'm currently experimenting with
-                    <a href="https://world.hey.com/tonysm">HEY World</a>, so check it out for new posts. Here's
+                    <a href="https://world.hey.com/tonysm">HEY World</a>, so check that out for new posts. Here's
                     some of my past writings:</p>
                 <div class="space-y-6">
                     @foreach($posts as $post)
