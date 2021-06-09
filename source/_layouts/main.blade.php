5d5e5d09878275d894aa15801bf1f026f2757d98
Tony Messias <tonysm@hey.com>
2021-06-08 21:43:17 -0300

    Add favicons

--- source/_layouts/main.blade.php
+++ source/_layouts/main.blade.php
@@ -5,11 +5,15 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="canonical" href="{{ $page->getUrl() }}">
         <meta name="description" content="{{ $page->description }}">
+        <link rel="apple-touch-icon" sizes="180x180" href="/assets/images/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/favicon-16x16.png">
+        <link rel="manifest" href="/assets/images/site.webmanifest">
         <title>{{ $page->title }}</title>
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <script defer src="{{ mix('js/main.js', 'assets/build') }}"></script>
     </head>
-    <body class="text-gray-900 font-sans antialiased text-lg leading-relaxed">
+    <body class="font-sans text-lg antialiased leading-relaxed text-gray-900">
         <div class="px-4">
             @yield('body')
         </div>
