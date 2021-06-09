0188b640f30ca950d79c9ed505ef9a8eaf15aef0
Tony Messias <tonysm@hey.com>
2021-06-08 23:25:24 -0300

    PHP 7.4

--- source/_partials/social.blade.php
+++ source/_partials/social.blade.php
@@ -1,12 +1,12 @@
 <meta name="twitter:card" content="summary_large_image">
 <meta name="twitter:site" content="@tonysmdev">
 <meta name="twitter:creator" content="@tonysmdev">
-<meta name="twitter:title" content="{{ $title ?? "Tony Messias" }}">
-<meta property="og:title" content="{{ $title ?? "Tony Messias" }}" />
-<meta property="og:url" content="{{ $page?->getUrl() }}" />
-<meta property="og:description" content="{{ $description ?? "Yo! I'm a programmer from Brazil sharing my thoughts and experiments over here."}}" />
-<meta name="twitter:description" content="{{ $description ?? "Yo! I'm a programmer from Brazil sharing my thoughts and experiments over here." }}">
-@if ($image ?? false)
+<meta name="twitter:title" content="{{ isset($title) && !empty($title) ? $title : "Tony Messias" }}">
+<meta property="og:title" content="{{ isset($title) && !empty($title) ? $title : "Tony Messias" }}" />
+<meta property="og:url" content="{{ $page->getUrl() }}" />
+<meta property="og:description" content="{{ isset($description) && !empty($description) ? $description : "Yo! I'm a programmer from Brazil sharing my thoughts and experiments over here."}}" />
+<meta name="twitter:description" content="{{ isset($description) && !empty($description) ? $description : "Yo! I'm a programmer from Brazil sharing my thoughts and experiments over here." }}">
+@if (isset($image))
 <meta property="og:image" content="{{ $image }}" />
 <meta name="twitter:image" content="{{ $image }}">
 @endif
